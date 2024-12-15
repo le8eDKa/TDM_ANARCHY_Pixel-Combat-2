d4cef53ef84060ca66458405faf20ebf00ce3ffe
le8eDKa <lebedv269@gmail.com>
2024-12-15 22:13:30 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -37,8 +37,7 @@ Room.Teams.OnRequestJoinTeam.Add(function(p, t) { // При нажатии на 
         if (p.id === '889D6F901662AB9B') {
                 p.Build.FlyEnable.Value = true;
                 p.inventory.MainInfinity.Value = true;
-        }
-});       
+        }       
         /*
                 Тут действия с игроком, который входит в команду, вот как это сделать:
 
