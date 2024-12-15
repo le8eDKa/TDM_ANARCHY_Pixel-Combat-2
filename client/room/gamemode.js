7283524ff8e5f85186f28e8b14165b424c76534b
BOSS-PIXELCOMBATS2 <144242217+BOSS-PIXELCOMBATS2@users.noreply.github.com>
2024-12-15 18:38:21 +0100

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,7 +1,8 @@
 import * as Basic from 'pixel_combats/basic';
 import * as Room from 'pixel_combats/room';
 
-CreateNewTeam('Blue', '<b><i>Синие</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Blue); // Создание команды с помощью функции в конце кода
+CreateNewTeam('Blue', '<b><i>Синие</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Blue); // Создание команд с помощью функции в конце кода
+CreateNewTeam('Red', '<b><i>Революционеры</i></b>', new Basic.Color(0, 0, 1, 0), 2, Room.BuildBlocksSet.Red);
 
 Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
         // При входе в команду
@@ -18,16 +19,6 @@ function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup
         NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
         return NewTeam;
 }
-
-CreateNewTeam('Red', '<b><i>революционеры</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Red); // Создание команды с помощью функции в конце кода
-
-Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
-        // При входе в команду
-        t.Add(p);
-});
-Room.Teams.OnPlayerChangeTeam.Add(function(p) {
-        p.Spawns.Spawn();
-});
 
 function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) { // Функция создания команды
         Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
