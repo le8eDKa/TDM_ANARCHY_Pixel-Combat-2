39ebcad93f67ca5368d91006269fce41c671169a
le8eDKa <lebedv269@gmail.com>
2024-12-15 20:37:18 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -18,3 +18,21 @@ function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup
         NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
         return NewTeam;
 }
+
+CreateNewTeam('Red', '<b><i>революционеры</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Red); // Создание команды с помощью функции в конце кода
+
+Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
+        // При входе в команду
+        t.Add(p);
+});
+Room.Teams.OnPlayerChangeTeam.Add(function(p) {
+        p.Spawns.Spawn();
+});
+
+function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) { // Функция создания команды
+        Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
+        const NewTeam = Room.Teams.Get(TeamName);
+        NewTeam.Spawns.SpawnPointsGroups.Add(TeamSpawnPointGroup);
+        NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
+        return NewTeam;
+}
