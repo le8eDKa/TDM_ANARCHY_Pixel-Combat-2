87845fd51c53107268368eaf7c3c141077207097
BOSS-PIXELCOMBATS2 <144242217+BOSS-PIXELCOMBATS2@users.noreply.github.com>
2024-12-15 18:08:58 +0100

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,2 +1,20 @@
-import * as Basik from 'pixel_combats/basic';
+import * as Basic from 'pixel_combats/basic';
 import * as Room from 'pixel_combats/room';
+
+CreateNewTeam('Blue', '<b><i>Синие</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Blue); // Создание команды с помощью функции в конце кода
+
+Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
+        // При входе в команду
+        t.Add(p);
+});
+Room.Teams.OnPlayerChangeTeam.Add(function(p) {
+        p.Spawns.Spawn();
+});
+
+function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) { // Функция создания команды
+        Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
+        const NewTeam = Room.Teams.Get(TeamName);
+        NewTeam.Spawns.SpawnPointsGroups.Add(TeamSpawnPointGroup);
+        NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
+        return NewTeam;
+}
