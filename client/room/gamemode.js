383324716d4d2a452b0a04e76cfaa3fb00926839
le8eDKa <lebedv269@gmail.com>
2024-12-15 21:41:16 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,30 +1,26 @@
 import * as Basic from 'pixel_combats/basic';
 import * as Room from 'pixel_combats/room';
 
-CreateNewTeam('Blue', '<b><i>Синие</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Blue); // Создание команд с помощью функции в конце кода
+// Создание команд с помощью функции в конце кода
+CreateNewTeam('Blue', '<b><i>Синие</i></b>', new Basic.Color(0, 0, 1, 0), 1, Room.BuildBlocksSet.Blue);
 CreateNewTeam('Red', '<b><i>Революционеры</i></b>', new Basic.Color(1, 0, 0, 0), 2, Room.BuildBlocksSet.Red);
 
 Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
-        // При входе в команду
-        if(player.id === '889D6F901662AB9B'){
-                player.flymode = true;
-        t.Add(p);
-});
-Room.Teams.OnPlayerChangeTeam.Add(function(p) {
-        p.Spawns.Spawn();
+    // При входе в команду
+    if (p.id === '889D6F901662AB9B') {
+        p.flymode = true;
+    }
+    t.Add(p);
 });
 
-function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) { // Функция создания команды
-        Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
-        const NewTeam = Room.Teams.Get(TeamName);
-        NewTeam.Spawns.SpawnPointsGroups.Add(TeamSpawnPointGroup);
-        NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
-        return NewTeam;
-}
-function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) { // Функция создания команды
-        Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
-        const NewTeam = Room.Teams.Get(TeamName);
-        NewTeam.Spawns.SpawnPointsGroups.Add(TeamSpawnPointGroup);
-        NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet;
-        return NewTeam;
+Room.Teams.OnPlayerChangeTeam.Add(function(p) {
+    p.Spawns.Spawn();
+});
+
+function CreateNewTeam(TeamName, TeamDisplayName, TeamColor, TeamSpawnPointGroup, TeamBuildBlocksSet) {
+    Room.Teams.Add(TeamName, TeamDisplayName, TeamColor);
+    const NewTeam = Room.Teams.Get(TeamName);
+    NewTeam.Spawns.SpawnPointsGroups.Add(TeamSpawnPointGroup);
+    NewTeam.Build.BlocksSet.Value = TeamBuildBlocksSet; // Убедитесь, что Value поддерживается
+    return NewTeam;
 }
