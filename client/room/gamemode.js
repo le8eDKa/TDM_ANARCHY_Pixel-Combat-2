83c5483e9ab1e954c64fcf38ea353258ed84c45a
le8eDKa <lebedv269@gmail.com>
2025-11-23 14:57:59 +0500

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -74,8 +74,7 @@ Room.Teams.OnRequestJoinTeam.Add(function(p, t) {
 Room.Teams.OnPlayerChangeTeam.Add(function(p) { 
         p.Spawns.Spawn();
 	if (p.id === 'AF89DB0FE9E8495F') p.PopUp('Привет НИКИТА >:)');
-	else p.PopUp(`Привет \'${p.NickName}\'!`);
-	else (p.id === 'D79592A274F501F7') p.PopUp('Привет Константигополь ;)');
+	else if (p.id === 'D79592A274F501F7') p.PopUp('Привет Константигополь ;)');
 	else p.PopUp(`Привет \'${p.NickName}\'!`);
 });
 Room.Spawns.GetContext().OnSpawn.Add(function(p) {
